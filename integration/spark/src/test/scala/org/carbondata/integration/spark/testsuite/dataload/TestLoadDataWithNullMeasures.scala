c7fbde596806eb4172b397d113c591203d9411ba
foryou2030 <foryou2030@126.com>
2016-05-13 15:45:34 +0800

    [issue-328]Data load failing when the measure data is null

---
+++ integration/spark/src/test/scala/org/carbondata/integration/spark/testsuite/dataload/TestLoadDataWithNullMeasures.scala
@@ -0,0 +1,25 @@
+package org.carbondata.integration.spark.testsuite.dataload
+
+import org.apache.spark.sql.common.util.CarbonHiveContext._
+import org.apache.spark.sql.common.util.QueryTest
+import org.scalatest.BeforeAndAfterAll
+
+/**
+ * Created by Administrator on 2016/5/17 0017.
+ */
+class TestLoadDataWithNullMeasures extends QueryTest with BeforeAndAfterAll {
+  override def beforeAll {
+    sql("CREATE CUBE carboncube DIMENSIONS (empno Integer, empname String, designation String, doj String, workgroupcategory Integer, workgroupcategoryname String, deptno Integer, deptname String, projectcode Integer, projectjoindate String, projectenddate String) MEASURES (attendance Integer,utilization Integer,salary Integer) OPTIONS (PARTITIONER [PARTITION_COUNT=1])")
+  }
+  test("test carbon table data loading when there are null measures in data") {
+    try {
+      sql("LOAD DATA FACT FROM './integration/spark/src/test/resources/datawithnullmsrs.csv' INTO CUBE carboncube OPTIONS(DELIMITER ',')");
+    } catch {
+      case e : Throwable => e.printStackTrace()
+    }
+  }
+
+  override def afterAll {
+    sql("drop cube carboncube")
+  }
+}
